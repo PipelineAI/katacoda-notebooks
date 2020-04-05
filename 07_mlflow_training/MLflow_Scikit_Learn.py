592af2bb9f080ae7df2334b9c8c465134e288800
Antje Barth <23338582+antje@users.noreply.github.com>
2020-04-05 21:42:07 +0200

    mlflow training and pipelines

---
+++ 07_mlflow_training/MLflow_Scikit_Learn.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pip install pandas==1.0.1
+
+
+# In[ ]:
+
+
+# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
+# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+import mlflow
+import mlflow.sklearn
+
+np.random.seed(40)
+warnings.filterwarnings("ignore")
+
+
+# # Setup Experiment Tracker
+
+# In[2]:
+
+
+tracking_uri='file:///root/mlflow'
+mlflow.set_tracking_uri(tracking_uri)
+
+experiment_name = 'wine'
+mlflow.set_experiment(experiment_name)    
+
+
+# # Import Training Data
+
+# In[ ]:
+
+
+# Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
+wine_path = './datasets/wine-quality.csv'
+data = pd.read_csv(wine_path)
+
+# Split the data into training and test sets. (0.75, 0.25) split.
+train, test = train_test_split(data)
+
+# The predicted column is "quality" which is a scalar from [3, 9]
+train_x = train.drop(["quality"], axis=1)
+test_x = test.drop(["quality"], axis=1)
+train_y = train[["quality"]]
+test_y = test[["quality"]]
+
+
+# # Start Training Run
+
+# In[ ]:
+
+
+alpha = 0.20
+l1_ratio = 0.20
+
+
+# In[ ]:
+
+
+with mlflow.start_run() as run:
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    rmse = np.sqrt(mean_squared_error(test_y, predicted_qualities))
+    mae = mean_absolute_error(test_y, predicted_qualities)
+    r2 = r2_score(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    # Log Parameters
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+
+    # Log Metrics
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+
+    # Log Model
+    mlflow.sklearn.log_model(lr, "model")
+
+
+# # Check the MLflow Pipelines Tab
+# ![MLflow Pipeline](https://raw.githubusercontent.com/PipelineAI/site/master/assets/img/mlflow-pipeline.png)
+
+# In[ ]:
+
+
+
+
---
+++ 07_mlflow_training/MLflow_TensorFlow.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''Trains and evaluate a simple MLP
+on the Reuters newswire topic classification task.
+'''
+from __future__ import print_function
+
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.saved_model import tag_constants
+
+import mlflow
+import mlflow.tensorflow
+from mlflow import pyfunc
+
+
+# # Setup Experiment Tracker
+
+# In[ ]:
+
+
+tracking_uri='file:///root/mlflow'
+mlflow.set_tracking_uri(tracking_uri)
+
+experiment_name = 'boston'
+mlflow.set_experiment(experiment_name)  
+
+import mlflow.tensorflow
+mlflow.tensorflow.autolog()
+
+
+# # Import Training Data 
+
+# In[ ]:
+
+
+# Builds, trains and evaluates a tf.estimator. Then, exports it for inference, logs the exported model
+# with MLflow, and loads the fitted model back as a PyFunc to make predictions.
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.boston_housing.load_data()
+# There are 13 features we are using for inference.
+feat_cols = [tf.feature_column.numeric_column(key="features", shape=(x_train.shape[1],))]
+feat_spec = {
+    "features": tf.placeholder("float", name="features", shape=[None, x_train.shape[1]])}
+hidden_units = [50, 20]
+steps = 1000
+regressor = tf.estimator.DNNRegressor(hidden_units=hidden_units, feature_columns=feat_cols)
+train_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_train}, y_train,
+                                                    num_epochs=None, shuffle=True)
+
+
+# # Start Training Run
+
+# In[ ]:
+
+
+with mlflow.start_run() as run:
+    mlflow.log_param("Hidden Units", hidden_units)
+    mlflow.log_param("Steps", steps)
+    regressor.train(train_input_fn, steps=steps)
+    test_input_fn = tf.estimator.inputs.numpy_input_fn({"features": x_test}, y_test,
+                                                       num_epochs=None, shuffle=True)
+    # Compute mean squared error
+    mse = regressor.evaluate(test_input_fn, steps=steps)
+    mlflow.log_metric("Mean Square Error", mse['average_loss'])
+    # Building a receiver function for exporting
+    receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feat_spec)
+    
+    saved_estimator_path = regressor.export_savedmodel('./saved-models/boston', receiver_fn).decode("utf-8")
+    # Logging the saved model
+    mlflow.tensorflow.log_model(tf_saved_model_dir=saved_estimator_path,
+                                tf_meta_graph_tags=[tag_constants.SERVING],
+                                tf_signature_def_key="predict",
+                                artifact_path="model")    
+
+
+# # Predict with the Model
+
+# In[ ]:
+
+
+# Resume the run by passing run id from above
+with mlflow.start_run(run_id=run.info.run_id) as run:
+    # Reload the model and predict
+    pyfunc_model = mlflow.pyfunc.load_model(mlflow.get_artifact_uri('model'))
+
+    # Predict with the loaded Python Function
+    df = pd.DataFrame(data=x_test, columns=["features"] * x_train.shape[1])
+    predict_df = pyfunc_model.predict(df)
+    predict_df['original_labels'] = y_test
+    print(predict_df)
+
+
+# # Check the MLflow Pipelines Tab
+# ![MLflow Pipeline](https://raw.githubusercontent.com/PipelineAI/site/master/assets/img/mlflow-pipeline3.png)
+
+# In[ ]:
+
+
+
+
