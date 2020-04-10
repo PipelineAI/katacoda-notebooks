371eb40f0b1c9cab77eaac34f9b0dcb69ebabae1
Antje Barth <23338582+antje@users.noreply.github.com>
2020-04-10 19:29:32 +0200

    Update mlflow_tracking.py

--- 06_mlflow_install/mlflow_tracking.py
+++ 06_mlflow_install/mlflow_tracking.py
@@ -1,7 +1,14 @@
 import os
 from random import random, randint
 
+import mlflow
 from mlflow import log_metric, log_param, log_artifacts
+
+tracking_uri='file:///root/mlflow'
+mlflow.set_tracking_uri(tracking_uri)
+
+experiment_name = 'hellow_world'
+mlflow.set_experiment(experiment_name)  
 
 if __name__ == "__main__":
     print("Running mlflow_tracking.py")
