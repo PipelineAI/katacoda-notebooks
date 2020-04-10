02a640d1a54c517c0ee45f170c7bcdee9d60745f
Antje Barth <23338582+antje@users.noreply.github.com>
2020-04-10 19:40:08 +0200

    Update mlflow_tracking.py

--- 06_mlflow_install/mlflow_tracking.py
+++ 06_mlflow_install/mlflow_tracking.py
@@ -13,15 +13,15 @@ mlflow.set_experiment(experiment_name)
 if __name__ == "__main__":
     print("Running mlflow_tracking.py")
 
-    log_param("param1", randint(0, 100))
+    log_param("hyperparam1", randint(0, 100))
 
-    log_metric("foo", random())
-    log_metric("foo", random() + 1)
-    log_metric("foo", random() + 2)
+    log_metric("accuracy", random())
+    log_metric("accuracy", random() + 1)
+    log_metric("accuracy", random() + 2)
 
     if not os.path.exists("outputs"):
         os.makedirs("outputs")
-    with open("outputs/test.txt", "w") as f:
+    with open("outputs/model.txt", "w") as f:
         f.write("hello world!")
 
     log_artifacts("outputs")
